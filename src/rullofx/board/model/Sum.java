8e14b0dd1d83452829b5d0947837dec7bcc0474a
Luigi Ribbi <luigi.ribbi@etudiant.univ-reims.fr>
2017-09-12 16:03:20 +0200

    Création des accesseurs, constructeurs  de la classe Sum + Création de l'interface Array.

--- src/rullofx/board/model/Sum.java
+++ src/rullofx/board/model/Sum.java
@@ -1,6 +1,31 @@
 package rullofx.board.model;
 
 public class Sum {
+	private int target;
+	private int current;
+	/**
+	 * @return the target
+	 */
+	public int getTarget() {
+		return target;
+	}
+	/**
+	 * @return the current
+	 */
+	public int getCurrent() {
+		return current;
+	}
+	/**
+	 * @param target
+	 */
+	public Sum(int target,Array<Cell> cells ) {
+		super();
+		this.target = target;
+		this.current = -1;
+	}
+	
+	
+	
 
 	
 }
