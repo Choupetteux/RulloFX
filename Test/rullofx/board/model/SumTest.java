6de3fe63cba6bcb49aaf9887c5d24310fef385a9
Luigi Ribbi <luigi.ribbi@etudiant.univ-reims.fr>
2017-09-12 16:06:10 +0200

    Création de la classeSumTest

---
+++ Test/rullofx/board/model/SumTest.java
@@ -0,0 +1,19 @@
+package rullofx.board.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SumTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
