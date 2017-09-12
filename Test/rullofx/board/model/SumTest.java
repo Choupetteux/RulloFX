dfbfde994a3ef821bf543bdf8be57d37bf2e51f1
Luigi Ribbi <luigi.ribbi@etudiant.univ-reims.fr>
2017-09-12 16:20:30 +0200

    Q7. Modification de la méthode update(), le test update_noInactiveCell() passe.

--- Test/rullofx/board/model/SumTest.java
+++ Test/rullofx/board/model/SumTest.java
@@ -7,13 +7,38 @@ import org.junit.Test;
 
 public class SumTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    /**
+     * Implémentation simple de l'interface Array dans une classe interne.
+     */
+    private class SimpleCellArray implements Array<Cell> {
+        Cell [] data = {new Cell(1), new Cell(2), new Cell(3), new Cell(4)};
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+        @Override
+        public int size() {
+            return data.length;
+        }
 
+        @Override
+        public Cell get(int index) {
+            return data[index];
+        }        
+    }
+    
+    private SimpleCellArray cells;
+    private Sum sum;
+
+    @Before
+    public void setUp() throws Exception {
+        this.cells = new SimpleCellArray();
+        this.sum = new Sum(5, this.cells);
+    }
+
+    @Test
+    public void update_noInactiveCell() {
+    	 sum.update();
+        
+        // toutes les cellules étant actives, la somme doit valoir 10
+        assertEquals(10, sum.getCurrent());
+    }
+    
 }
--- src/rullofx/board/model/Sum.java
+++ src/rullofx/board/model/Sum.java
@@ -3,6 +3,8 @@ package rullofx.board.model;
 public class Sum {
 	private int target;
 	private int current;
+	private Array<Cell> cells;
+	
 	/**
 	 * @return the target
 	 */
@@ -22,6 +24,7 @@ public class Sum {
 		super();
 		this.target = target;
 		this.current = -1;
+		this.cells = cells;
 	}
 
     /**
@@ -36,6 +39,15 @@ public class Sum {
      * Met à jour la somme courante, en ne tenant compte que des cellules actives.
      */
     public void update() {
+    	this.current = 0;
+    	//Pour chaque cellule
+    	for(int i = 0; i < this.cells.size(); i++){
+    		//qui sont actives
+    		if(this.cells.get(i).isActive()){
+    			//Ajouter la valeur de la cellule à la somme courante.
+    			this.current = this.current + this.cells.get(i).getValue();
+    		}
+    	}
     }        
     
 	
