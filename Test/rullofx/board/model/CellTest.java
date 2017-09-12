1f17ffa30bf9e3c340e0e672bf387c099158f19a
Luigi Ribbi <luigi.ribbi@etudiant.univ-reims.fr>
2017-09-12 15:47:22 +0200

    Q10. Ces tests sont le déverrouillage d'une cellule et la désactivation d'une cellule verrouillée, les tests sont écrits.

--- Test/rullofx/board/model/CellTest.java
+++ Test/rullofx/board/model/CellTest.java
@@ -16,7 +16,7 @@ public class CellTest {
 
     /**
      * Ce test vérifie le comportement de la méthode toggleActiveState sur une
-     * cellule active et dévérouillée.
+     * cellule active et déverrouillée.
      */
     @Test
     public void toggleActiveState_activeAndUnlockedCell() {
@@ -30,6 +30,7 @@ public class CellTest {
         assertFalse("Etat de la cellule incorrect", cell.isActive());
     }
     
+    /** Essayer d'activer une cellule inactive */
     @Test
     public void toggleActiveState_inactiveCell(){
     	cell.toggleActiveState();
@@ -37,6 +38,16 @@ public class CellTest {
     	assertTrue("La cellule devrait être activée",cell.isActive());
     }
     
+    /** Essayer de désactiver une cellule verrouillée */
+    @Test
+    public void toggleActiveState_activeAndLockedCell(){
+    	cell.toggleLockedState();
+    	boolean returnValue = cell.toggleActiveState();
+    	assertFalse("L'état de la cellule ne devrait pas être modifiable", returnValue);
+    	assertTrue("La cellule devrait toujours être active", cell.isActive());
+    }
+    
+    /** Essayer de verrouiller une cellule active */
     @Test
     public void toggleLockedState_activeAndUnlockedCell(){
     	boolean returnValue = cell.toggleLockedState();
@@ -49,10 +60,21 @@ public class CellTest {
         assertTrue("Etat de la cellule incorrect", cell.isLocked());
     }
     
+    /** Essayer de verrouiller une cellule inactive */
     @Test
     public void toggleLockedState_inactiveCell(){
     	cell.toggleActiveState();
     	assertFalse("L'état de la cellule ne devrait pas être modifié",cell.toggleLockedState());
     	assertFalse("La cellule ne devrait pas être verrouillé.",cell.isLocked());
     }
+    
+    /** Essayer de déverrouiller une cellule verrouillée. */
+    @Test
+    public void toggleLockedState_lockedCell(){
+    	cell.toggleLockedState();
+    	boolean returnValue = cell.toggleLockedState();
+    	assertTrue("L'état de la cellule n'a pas pu être modifié", returnValue);
+    	assertFalse("La cellule n'est pas déverrouillée", cell.isLocked());
+    }
+  
 }
