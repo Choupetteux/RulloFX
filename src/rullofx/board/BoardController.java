92dc7bf9d7e6dae3bcd59cbdc8f0f4dcc9e06e97
Luigi Ribbi <luigi.ribbi@etudiant.univ-reims.fr>
2017-11-07 12:44:10 +0100

    Méthode onCellClicked.

--- src/rullofx/board/BoardController.java
+++ src/rullofx/board/BoardController.java
@@ -3,10 +3,18 @@
  */
 package rullofx.board;
 
+import javafx.scene.input.MouseEvent;
+
 public class BoardController {
     private BoardView boardView;
 
     public BoardController(BoardView boardView) {
         this.boardView = boardView;
+    }
+    
+    public void onCellClicked(MouseEvent event, int row, int column){
+    	System.out.println(row);
+    	System.out.println(column);
+    	System.out.println(event);
     }
 }
