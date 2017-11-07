abaf35aa588239abfcd0f47ba711c4453d5e911b
Luigi Ribbi <luigi.ribbi@etudiant.univ-reims.fr>
2017-11-07 10:57:24 +0100

    Ajout du stackpane au FXML, ajout du stackPane dans le code.

--- src/rullofx/RulloController.java
+++ src/rullofx/RulloController.java
@@ -9,47 +9,47 @@ public class RulloController {
 	@FXML
 	StackPane stackPane;
 
-	private BoardView boardView;//= new BoardView();
+	private BoardView boardView;
 
 	/**
 	 * Méthode invoquée automatiquement après la création de l'interface graphique
 	 * associée à ce contrôleur.
 	 */
-	/*public void initialize() {
-		BoardView boardView=new BoardView();
-		stackPane.getChildren().add(boardView = new BoardView());
-
-		public EventType enumeration=new EventType();
-		switch(enumeration){
-		case START_EVENT:
-			this.boardView.init();
-			break;
-		case ACTIVATION_EVENT:
-			break;
-		case LOCK_EVENT:
-			break;
-		case REACHED_COLUMN_EVENT:
-			break;
-		case REACHED_ROW_EVENT:
-			break;
-		case SOLVED_EVENT:
-			break;
-		}
+	public void initialize() {
+		stackPane.getChildren().add(boardView = new BoardView());        
 	}
 
+	public EventType enumeration=new EventType();
+	switch(enumeration){
+	case START_EVENT:
+		this.boardView.init();
+		break;
+	case ACTIVATION_EVENT:
+		break;
+	case LOCK_EVENT:
+		break;
+	case REACHED_COLUMN_EVENT:
+		break;
+	case REACHED_ROW_EVENT:
+		break;
+	case SOLVED_EVENT:
+		break;
+	}
+}
 
 
 
-	public Class CellView(){
-		private int row;
-		private int column;
 
-		public CellView(int row, int col){
-			this.row=row;
-			this.column=col;
-		}
+public Class CellView(){
+	private int row;
+	private int column;
 
-	}*/
+	public CellView(int row, int col){
+		this.row=row;
+		this.column=col;
+	}
+
+}*/
 }
 
 
